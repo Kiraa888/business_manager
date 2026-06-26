ec07c07703460bf31354b558637e36eecfbb0f29
Animesh Bauri <159995350+Kiraa888@users.noreply.github.com>
2026-06-26 19:15:14 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'clientflow-cache-v2';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://iili.io/CAc9hEG.png',
+  'https://iili.io/CAcJQX1.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
